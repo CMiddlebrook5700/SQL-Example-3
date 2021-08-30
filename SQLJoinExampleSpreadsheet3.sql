be2817cb3720323caa1adfec202437175e7e311f
CMiddlebrook5700 <60832355+CMiddlebrook5700@users.noreply.github.com>
2021-08-29 22:32:45 -0700

    Query to pull the data and export of the data.

---
+++ SQLJoinExampleSpreadsheet3.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT player_name, position FROM player_name LEFT JOIN gameday_performance 
+ON player_name.player_number=gameday_performance.player_number
+WHERE td_catch > 3 AND college = 'LSU'
+ORDER BY player_name ASC
+LIMIT 0, 1000
+
+-- Date: 2021-08-29 22:29
+*/
+INSERT INTO `` (`player_name`,`position`) VALUES ('jarvis_laundry','WR');
+INSERT INTO `` (`player_name`,`position`) VALUES ('odell_beckham_jr','WR');
